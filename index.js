d09f6cbd61cede79c3c382a1aaf1818a7f5361fc
Md. Sumsuzzoha <mdzoha11@gmail.com>
2024-01-07 23:40:56 +0600

    jwt add

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-require('dotenv').config();
+
 
 
 //middleware
@@ -33,20 +35,93 @@ async function run() {
         const cartCollection = client.db("bistroDB").collection("carts");
         const userCollection = client.db("bistroDB").collection("users");
 
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(
+                user,
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '1h' });
+            res.send({ token });
+        })
+        // ==================
+        // varify token with middleware
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'Unauthorized Access' });
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(403).send({ message: 'Forbidden Access' })
+                }
+                req.decoded = decoded;
+                next();
+            });
+        }
+        // varify admin token with middleware
+        const verifyAdmin = async (req, res) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (isAdmin) {
+                return res.status(403).send({ message: 'Forbidden Access' });
+            }
+            next();
+        }
+        // =====================
 
         // users related api
         app.post('/users', async (req, res) => {
             const user = req.body;
             // insert if email if dosenot exist
-            const query = {email: user.email}
+            const query = { email: user.email }
             const existingUser = await userCollection.findOne(query);
-            if(existingUser){
-                return res.send({message: 'user is already exist', insertedId: null})
+            if (existingUser) {
+                return res.send({ message: 'user is already exist', insertedId: null })
             }
             const result = await userCollection.insertOne(user);
             res.send(result);
         })
 
+        app.get('/users', verifyToken, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: "UnAuthorized Access" })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send( {admin} );
+        })
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            // const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    role: `admin`
+                },
+            };
+            const result = await userCollection.updateOne(query, updateDoc);
+            res.send(result);
+
+
+        })
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
         // ========================
 
         // menu related api
